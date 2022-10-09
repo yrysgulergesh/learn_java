a820b0f93f6692bc1bf29a61b88264bc26a3fad2
Yrysgul Ergesh <yrysgulergesh@gmail.com>
2022-10-09 12:41:19 +0600

    start java

---
+++ com/epam/learn/main/FirstProgram.java
@@ -0,0 +1,10 @@
+package com.epam.learn.main;
+
+public class FirstProgram {
+    public static void main(String[] args) {
+        System.out.println("Hello Java!");
+        System.out.println(9);
+        System.out.println("Java " +19);
+        System.out.println("Hi, Yrysgul");
+    }
+}
